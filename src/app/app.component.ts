b3ae47be584b13f140347fd6cd2a4489bd4c6ca2
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-27 16:14:05 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,14 @@ import { CalendarComponent } from "./calendar/calendar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalendarComponent],
+  imports: [RouterOutlet,CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngcalendar';
+  meetings = {
+    '2024-03-05': ['Dring Coffee', 'Learn React', 'Sleep'],
+    '2024-03-06': ['Dring Coffee', 'Learn Angular', 'Sleep'],
+  };
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,21 +1,24 @@
-import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { Component, InputSignal, Signal, WritableSignal, computed, input, signal } from '@angular/core';
 import { DateTime, Info, Interval } from 'luxon';
+import { CommonModule } from '@angular/common';
+import { Meetings } from './meeetings.interface';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
 
+  meetings:InputSignal<Meetings>=input.required();
   today: Signal<DateTime>=signal(DateTime.local());
 
   fistDayOfActiveMonth: WritableSignal<DateTime>=signal(
     this.today().startOf('month'),
-
   );
 
+  activeDay:WritableSignal<DateTime | null>=signal(null);
   weekDays:Signal<string[]>=signal(Info.weekdays('short'));
   daysOfMonth:Signal<DateTime[]>=computed(()=>{
     return Interval.fromDateTimes(
@@ -32,7 +35,20 @@ export class CalendarComponent {
     })
   });
 
+  DATE_MED=DateTime.DATE_MED;
+  activateDayMeetings:Signal<string[]>=computed (()=>{
+    const activeDay=this.activeDay();
+    if(activeDay===null){
+      return[]
+    }
+    const activeDayISO=activeDay.toISODate()
 
+
+    if(!activeDayISO){
+      return[];
+    }
+    return this.meetings()[activeDayISO]??[];
+  })
   constructor(){
     console.log(this.daysOfMonth())
   }
